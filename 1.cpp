d76368fe33b5306b1235c857ff6bb32f6327cb9f
Quenassier <112952435+Quenassier@users.noreply.github.com>
2022-12-06 17:00:25 +0700

    Update 1.cpp

--- 1.cpp
+++ 1.cpp
@@ -8,15 +8,15 @@ int number(int b)
     int count = 1;
     for (int i = 1; i < b; i++)
     {
-        
+
         if (b % i == 0)
         {
-            
+
             count++;
         }
-        
+
     }
-    return count;   
+    return count;
 }
 
 int main()
@@ -25,56 +25,42 @@ int main()
 
     ifstream f_input;
     ofstream f_output;
-   ofstream result("result.txt ");
+    ofstream result("result.txt ");
 
     int count = 0;
     int numberInTheFile;
     int num;
-    
+
     do
     {
         cout << "Введите целое число: ";
-       cin >> num;
+        cin >> num;
 
         if (num != 0)
         {
-            f_output.open("R.txt" );
+            f_output.open("R.txt");
 
-            if (!f_output.is_open())
-            {
-                cout << "Ошибка открытия файла";
-                return 1;
-            }
-            else
-            {
                 f_output << num << '\n';
                 f_output.close();
-            }
         }
         else
         {
             cout << "Последовательность завершена!";
         }
+
     } while (num != 0);
 
-    f_input.open("R.txt", ios_base::app);
+    f_input.open("R.txt");
 
-    if (!f_input.is_open())
-    {
-        cout << "Ошибка открытия файла";
-        return 1;
-    }
-    else
-    {
         while (f_input >> numberInTheFile)
         {
-            
+
             result << number(numberInTheFile) << endl;
-                   
+
         }
-       result << "Общие количество простых чисел в последовательности равно: " << count;
-       f_input.close();
-    }
+        result << "Общие количество простых чисел в последовательности равно: " << count;
+        f_input.close();
+
     return 0;
 
 }
