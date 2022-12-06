fd2efbc3ab7d9c93100f2bd0ef2476cabe30a2d7
Quenassier <112952435+Quenassier@users.noreply.github.com>
2022-12-06 15:52:52 +0700

    Update 1.cpp

--- 1.cpp
+++ 1.cpp
@@ -3,26 +3,78 @@
 #include <locale.h>
 using namespace std;
 
-void all(int x);
-
-int main(){
-    setlocale(LC_ALL,"Russian");
-    int number;
-    ifstream f1;
-    f1.open("numbers.txt");
-    
-    while(!f1.eof()){
-        f1>>number;
-        all(number);
-   }
-    f1.close();
+int number(int b)
+{
+    int count = 1;
+    for (int i = 1; i < b; i++)
+    {
+        
+        if (b % i == 0)
+        {
+            
+            count++;
+        }
+        
+    }
+    return count;   
 }
-void all(int x){
-    int number1 = x/100 ,number2 = (x/10)%10 ,number3 = x%10;
-    int sum,proiz;
-    double arethm;
-    sum = number1 + number2 + number3;
-    proiz = number1 * number2 * number3;
-    arethm = (number1 + number2 + number3)/3;
-    printf("summa - %d,umnozhenie - %d,areth - %0.2lf \n",sum,proiz,arethm);
+
+int main()
+{
+    setlocale(LC_ALL, "ru");
+
+    ifstream f_input;
+    ofstream f_output;
+   ofstream result("result.txt ");
+
+    int count = 0;
+    int numberInTheFile;
+    int num;
+    
+    do
+    {
+        cout << "Введите целое число: ";
+       cin >> num;
+
+        if (num != 0)
+        {
+            f_output.open("R.txt" );
+
+            if (!f_output.is_open())
+            {
+                cout << "Ошибка открытия файла";
+                return 1;
+            }
+            else
+            {
+                f_output << num << '\n';
+                f_output.close();
+            }
+        }
+        else
+        {
+            cout << "Последовательность завершена!";
+        }
+    } while (num != 0);
+
+    f_input.open("R.txt", ios_base::app);
+
+    if (!f_input.is_open())
+    {
+        cout << "Ошибка открытия файла";
+        return 1;
+    }
+    else
+    {
+        while (f_input >> numberInTheFile)
+        {
+            
+            result << number(numberInTheFile) << endl;
+                   
+        }
+       result << "Общие количество простых чисел в последовательности равно: " << count;
+       f_input.close();
+    }
+    return 0;
+
 }
